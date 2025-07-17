a41489147b3d5f12ef9f2af0e4d78c6961c4bd24
biancapcr <biancacristina.pocora@gmail.com>
2025-07-17 15:57:41 +0200

    snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+// Trova lo studente con id === 2
+const student = students.find(s => s.id === 2);
+
+console.log(student);
